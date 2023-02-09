dd429cf54b23fe017cfa0a237cc55c62e6df97c3
Jonathan Davis <trunten@btinternet.com>
2023-02-09 17:36:27 +0000

    change chart data on click test

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,41 +1,34 @@
+let chart;
 function updateGraph(data) {
-    data = data || [
-      { category: "Crime 1", count: 10 },
-      { category: "Crime 2", count: 20 },
-      { category: "Crime 3", count: 15 },
-      { category: "Crime 4", count: 25 },
-      { category: "Crime 5", count: 22 },
-      { category: "Crime 6", count: 30 },
-      { category: "Crime 7", count: 28 },
-    ];
+    let label = 'Crime frequency';
 
-    const config = {
-        type: 'radar',
-        data: data,
-        options: {
-          elements: {
-            line: {
-              borderWidth: 3
-            }
-          }
-        },
-      };
+    data = data || [
+      { category: "Crime 1", count: Math.floor(Math.random() * 36) + 5 },
+      { category: "Crime 2", count: Math.floor(Math.random() * 26) + 5 },
+      { category: "Crime 3", count: Math.floor(Math.random() * 26) + 5 },
+      { category: "Crime 4", count: Math.floor(Math.random() * 26) + 5 },
+      { category: "Crime 5", count: Math.floor(Math.random() * 26) + 5 },
+      { category: "Crime 6", count: Math.floor(Math.random() * 26) + 5 },
+      { category: "Crime 7", count: Math.floor(Math.random() * 26) + 5 },
+    ];
   
-    new Chart(
-      document.getElementById('crime-graph'),
-      {
-        type: 'radar',
-        data: {
-          labels: data.map(row => row.category),
-          datasets: [
-            {
-              label: 'Crime frequency',
-              data: data.map(row => row.count)
+    if (chart) { chart.destroy() }
+    chart = new Chart(
+        document.getElementById('crime-graph'),
+        {
+            type: 'radar',
+            data: {
+                labels: data.map(row => row.category),
+                datasets: [
+                    {
+                        label: label,
+                        data: data.map(row => row.count)
+                    }
+                ]
             }
-          ]
-        }
-      }
-    );
+        });
   }
 
   updateGraph();
+
+  document.querySelector("#nav").addEventListener("click", ()=>updateGraph());
