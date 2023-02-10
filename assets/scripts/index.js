93c0dfca976cd22ed08d7b7c58eb90bd046c9bc3
logarythm <mahmood.ihsan98989@gmail.com>
2023-02-10 17:02:18 +0000

    added country and population to city section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -158,6 +158,8 @@ function cityapi(cityname){
         headers: { 'X-Api-Key': 'go1NeuJz94QeGftEPbnsgg==hvklAeK6cSwQ06Ti'}
     }).then(function(response){
         console.log(response);
+        $(".heading").text(response[0].name);
+        $(".content").text("Country : " + response[0].country + "  Population : " + (response[0].population).toLocaleString("en-US"));
     });
 }
 
