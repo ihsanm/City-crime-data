da5cb10f2b56fd2a239f72d9cbae941f7ef50e9e
logarythm <mahmood.ihsan98989@gmail.com>
2023-02-14 19:14:00 +0000

    added several link to city description

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -145,6 +145,10 @@ function policeforce(policeforcetext){
         info.empty();
         if (response.description) info.html(response.description);
         info.append($(`<a href="${response.url}" target="_blank">${response.url}</a>`));
+
+        for( i=0 ; i< response.engagement_methods.length ; i++){
+        info.append($("<br>" + `<a href="${response.engagement_methods[i].url}" target="_blank">${response.engagement_methods[i].url}</a>`));
+        }
         info.append("<br>" + " Telephone : " + response.telephone)
     });
 }
